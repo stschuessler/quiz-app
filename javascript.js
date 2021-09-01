02bfa07217f16de0317150bb400a307ae9eb1a17
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-01 18:55:57 +0200

    add addEventListener + test with alert

--- javascript.js
+++ javascript.js
@@ -1,7 +1,3 @@
-//let text = 'Hello World!'
-//alert(text)
-//console.log(text)
-
 /* Befehl für den Zugriff auf die jeweiligen id-Elemente + 
 Zuweisung auf eine Variable. Hier: main-Elemente */
 const questionContent = document.querySelector('#questions')
@@ -15,3 +11,7 @@ const questionButton = document.querySelector('#question-button')
 const bookmarkButton = document.querySelector('#bookmark-button')
 const createButtom = document.querySelector('#create-button')
 // const profileElement = document.querySelector("#profile-button");
+
+questionButton.addEventListener('click', () => {
+  alert('hallo')
+})
