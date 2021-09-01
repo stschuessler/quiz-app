814a6b56ad680ca7185154db8213e9ee47698a95
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-01 18:48:04 +0200

    add querySelector and create Variables

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
 //let text = 'Hello World!'
 //alert(text)
 //console.log(text)
+
+/* Befehl für den Zugriff auf die jeweiligen id-Elemente + 
+Zuweisung auf eine Variable. Hier: main-Elemente */
+const questionContent = document.querySelector('#questions')
+const bookmarksContent = document.querySelector('#bookmarks')
+const createContent = document.querySelector('#create')
+//const  = document.querySelector("#profile");
+
+/*Befehl für den Zugriff auf die jeweiligen id-Elemente + 
+Zuweisung auf eine Variable. Hier: button-Elemente im footer*/
+const questionButton = document.querySelector('#question-button')
+const bookmarkButton = document.querySelector('#bookmark-button')
+const createButtom = document.querySelector('#create-button')
+// const profileElement = document.querySelector("#profile-button");
